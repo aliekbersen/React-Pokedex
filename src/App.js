f294c4d20fb44e7ddecd65f53d9ec52edbe567d9
RexenN <96340446+RexenN@users.noreply.github.com>
2022-05-22 20:35:55 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="head">
+      <h1>Pokemon Codex</h1>
+      <div className='container'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pokemon-thumb.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pokemonthumb = () => {
+  return (
+    <div>pokemon-thumb</div>
+  )
+}
+
+export default pokemon-thumb
